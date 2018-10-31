cf5883c1830e0220ce409d1121b8e9477bc0599a
Daniel <daniel.zuziak@gmail.com>
2018-10-31 11:00:57 +0000

    Initial commit

---
+++ cos.py
@@ -0,0 +1,17 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+	ret, frame = cap.read()
+
+	edges = cv2.Canny(frame, 100, 200)
+
+	cv2.imshow('frame', edges)
+
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+cap.release()
+cv2.destroyAllWindows()
+
