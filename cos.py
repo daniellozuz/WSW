8eb3ff0c6efbcf257b19879b19821162013ea9e6
Daniel <daniel.zuziak@gmail.com>
2018-11-07 11:26:13 +0000

    zmiana progu

--- cos.py
+++ cos.py
@@ -8,7 +8,7 @@ def draw_contours(image):
     ratio = image.shape[0] / float(resized.shape[0])
     gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
     blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-    thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
+    thresh = cv2.threshold(blurred, 100, 255, cv2.THRESH_BINARY_INV)[1]
 
     cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
     cv2.CHAIN_APPROX_SIMPLE)
