34a8d8eed352c2d6211baa4596e56f5d9594b78e
Daniel Zuziak <daniel.zuziak@gmail.com>
2018-11-07 12:08:22 +0100

    cleaned

--- cos.py
+++ cos.py
@@ -4,33 +4,22 @@ from shapedetector import ShapeDetector
 
 
 def draw_contours(image):
-    # the shapes can be approximated better
-    #image = cv2.imread(image)
     resized = imutils.resize(image, width=300)
     ratio = image.shape[0] / float(resized.shape[0])
-    # convert the resized image to grayscale, blur it slightly,
-    # and threshold it
     gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
     blurred = cv2.GaussianBlur(gray, (5, 5), 0)
     thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
-     
-    # find contours in the thresholded image and initialize the
-    # shape detector
+
     cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
     cv2.CHAIN_APPROX_SIMPLE)
     cnts = cnts[0] if imutils.is_cv2() else cnts[1]
 
     sd = ShapeDetector()
     for c in cnts:
-        # compute the center of the contour, then detect the name of the
-        # shape using only the contour
         M = cv2.moments(c)
         
         shape = sd.detect(c)
 
-        
-        # multiply the contour (x, y)-coordinates by the resize ratio,
-        # then draw the contours and the name of the shape on the image
         c = c.astype("float")
         c *= ratio
         c = c.astype("int")
@@ -43,7 +32,6 @@ def draw_contours(image):
         except ZeroDivisionError:
             pass
      
-        # show the output image
     cv2.imshow("Image", image)
     
 
@@ -52,15 +40,11 @@ cap = cv2.VideoCapture(0)
 
 while(True):
     ret, frame = cap.read()
-    print(frame)
 
     draw_contours(frame)
-
-    #cv2.imshow('frame', edges)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
 cv2.destroyAllWindows()
-
