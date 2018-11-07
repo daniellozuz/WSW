e89509e0568b9fd21641de2f5c1efc78e5dbcdd0
Daniel <daniel.zuziak@gmail.com>
2018-11-07 11:06:48 +0000

    shape detection

--- cos.py
+++ cos.py
@@ -1,18 +1,65 @@
 import cv2
-from skimage import feature
-import numpy as np
+import imutils
+from shapedetector import ShapeDetector
+
+
+def draw_contours(image):
+    # the shapes can be approximated better
+    #image = cv2.imread(image)
+    resized = imutils.resize(image, width=300)
+    ratio = image.shape[0] / float(resized.shape[0])
+    # convert the resized image to grayscale, blur it slightly,
+    # and threshold it
+    gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
+     
+    # find contours in the thresholded image and initialize the
+    # shape detector
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+    cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+
+    sd = ShapeDetector()
+    for c in cnts:
+        # compute the center of the contour, then detect the name of the
+        # shape using only the contour
+        M = cv2.moments(c)
+        
+        shape = sd.detect(c)
+
+        
+        # multiply the contour (x, y)-coordinates by the resize ratio,
+        # then draw the contours and the name of the shape on the image
+        c = c.astype("float")
+        c *= ratio
+        c = c.astype("int")
+        cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+        try:
+            cX = int((M["m10"] / M["m00"]) * ratio)
+            cY = int((M["m01"] / M["m00"]) * ratio)
+            cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
+                0.5, (255, 255, 255), 2)
+        except ZeroDivisionError:
+            pass
+     
+        # show the output image
+    cv2.imshow("Image", image)
+    
+
 
 cap = cv2.VideoCapture(0)
 
 while(True):
-	ret, frame = cap.read()
+    ret, frame = cap.read()
+    print(frame)
 
-	edges = cv2.Canny(frame, 100, 200)
+    draw_contours(frame)
 
-	cv2.imshow('frame', edges)
+    #cv2.imshow('frame', edges)
 
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 cap.release()
 cv2.destroyAllWindows()
